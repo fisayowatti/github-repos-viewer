b1694cb5b49df6ab1f38533f46c8525c85460504
Fisayo <48415629+fisayowatti@users.noreply.github.com>
2021-05-22 12:29:10 +0100

    add a form that sends the username to the Repos screen

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router';
 
 export default function Search() {
+  const [value, setValue] = useState("");
+  const history = useHistory();
   return (
-    <div>
-      This is Search
+    <div className="min-h-screen p-4">
+      <input value={value} onChange={(e) => setValue(e.target.value)} placeholder='see me here na' />
+      <button onClick={() => history.push("/repos", { username: value })}>Click me</button>
     </div>
   )
 }

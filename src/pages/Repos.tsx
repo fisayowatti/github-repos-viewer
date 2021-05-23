7d12be91586a17592859d165c01a3b47aca17375
Fisayo <48415629+fisayowatti@users.noreply.github.com>
2021-05-23 19:19:12 +0100

    Correctly  format and render all available data

--- src/pages/Repos.tsx
+++ src/pages/Repos.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useLocation } from 'react-router'
 import { useQuery } from 'urql'
+import * as dayjs from 'dayjs'
+import * as relativeTime from 'dayjs/plugin/relativeTime' 
 
 const ReposQuery = `query($username:String!) { 
   user(login: $username) {
@@ -20,10 +22,16 @@ const ReposQuery = `query($username:String!) {
         },
         viewerHasStarred,
         updatedAt,
+        licenseInfo {
+					name,
+        },
+        forkCount,
       }
     }
   }
 }`
+
+dayjs.extend(relativeTime)
 
 export default function Repos() {
 
@@ -42,39 +50,47 @@ export default function Repos() {
 
   const {data, fetching, error} = result;
 
-
-  console.log('res', data?.user, location.state )
+  console.log('res', data?.user, location.state, dayjs(data?.user?.repositories?.nodes?.[0]?.updatedAt).fromNow() )
   return (
-    <div className="bg-white min-h-screen flex flex-row p-8">
-      <div className="w-3/12">
-        <div className="relative">
+    <div className="bg-white min-h-screen p-4 md:flex md:p-8 md:space-x-8 mx-auto max-w-full md:max-w-screen-xl">
+      <div className="flex items-center md:w-3/12 md:flex-col md:items-start">
+        <div className="relative w-1/5 md:w-full">
           <div className="rounded-full overflow-hidden">
-            <img className="w-full" src={data?.user.avatarUrl}/>
+            <img className="" src={data?.user.avatarUrl}/>
           </div>
-          <div className="absolute bottom-12 right-4 h-8 w-8 bg-gray-400 rounded-full"></div>
+          <div className="absolute bottom-12 right-4 h-8 w-8 bg-gray-400 rounded-full hidden md:block"></div>
         </div> {/** IMAGE ENDS HERE  */}
-        <div className="text-3xl font-bold">{data?.user.name}</div>
-        <div className="text-l text-gray-500">{data?.user.login}</div>
-        <div className="text-l text-gray-500">{data?.user.bio}</div>
+        <div className="flex-col ml-4 md:ml-0 md:mt-4">
+          <div className="text-2xl font-bold">{data?.user.name}</div>
+          <div className="text-l text-gray-500">{data?.user.login}</div>
+          <div className="text-l text-gray-500">{data?.user.bio}</div>
+        </div>
       </div> {/** LEFT SIDE ENDS HERE */}
-      <div>
+      <div className="md:w-9/12">
         {data?.user.repositories.nodes.map(repo => (
-          <div key={repo.id} className="border">
-            <div className="flex flex-row justify-between">
+          <div key={repo.id} className="border-b pt-6 pb-3">
+            <div className="flex flex-row justify-between mb-4 ">
               <div>
-                <a href="#"> {repo?.name} </a>
-                <div>{repo?.description}</div>
+                <a href="#" className="text-blue-600 font-bold text-xl hover:underline"> {repo?.name} </a>
+                <div className="text-sm text-gray-600 mt-1 max-w-prose">{repo?.description}</div>
               </div>
-              <button className="">Star</button>
+              <button className="px-4 py-1 bg-gray-100 text-sm font-medium self-center rounded-lg border">{repo.viewerHasStarred ? 'Unstar' : 'Star'}</button>
             </div>
-            <div className="flex flex-row space-x-3">
+            <div className="flex flex-row space-x-3 text-xs text-gray-700">
               { repo?.primaryLanguage && 
                 <div className="flex flex-row items-center">
-                  <div className="h-3 w-3 rounded-xl" style={{backgroundColor: repo?.primaryLanguage?.color}}></div>
+                  <div className="h-3 w-3 rounded-xl mr-0.5" style={{backgroundColor: repo?.primaryLanguage?.color}}></div>
                   <div>{repo?.primaryLanguage?.name}</div>
                 </div>
               }
-              <div>{repo.updatedAt}</div>
+              { repo?.forkCount !== 0 &&
+                <div>{repo?.forkCount}</div>
+              }
+              { repo?.licenseInfo &&
+                <div>{repo?.licenseInfo?.name}</div>
+
+              }
+              <div>{`Updated ${dayjs(repo?.updatedAt).fromNow()}`}</div>
             </div>
           </div>
         ))}
